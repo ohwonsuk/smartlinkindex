1b860a838a8efa28af3c6e6929cd272021f58455
ohwonsuk <102600489+ohwonsuk@users.noreply.github.com>
2024-01-18 10:43:11 +0900

    버튼 추가 오류 수정(버튼명 동일이름 오류건)

--- app.py
+++ app.py
@@ -144,51 +144,54 @@ if uploaded_file is not None:
 
     st.write('업로드 전체 데이터수: ',cms_raw['carId'].count())
     st.write("---")
-    if st.button("분석하기"):
-      #시간 속성 필드값 datetime으로 type 변경
-      cms_raw['차량생성일시'] = cms_raw['차량생성일시'].astype('datetime64[ns]')
-      cms_raw['현재장착일'] = cms_raw['현재장착일'].astype('datetime64[ns]')
-      cms_raw['마지막시동on'] = cms_raw['마지막시동on'].astype('datetime64[ns]')
-      cms_raw['DEV_EUI'].fillna(0,inplace=True)
-      cms_raw['차량번호(clean)'] = cms_raw['차량번호'].apply(carnoclean)
+else:
+    st.warning('엑셀파일을 업로드 하세요')
 
-      # 유효한 차량 - 차량사용여부 '1', 단말기 일련번호 있는 차량과 HMG API(서비스유형 '8') 조건
-      car_available = cms_raw.loc[((cms_raw['차량사용여부'] == 1) & (cms_raw['DEV_EUI'] != 0)) | (cms_raw['서비스유형'] == 8) ]
-      st.write('유효한 차량대수:' , car_available['carId'].count())
-      st.write('차량사용여부, 단말기 장착, HMG API 차량 포함')
-      st.write("---")
-      car_check_list = cms_raw.loc[(cms_raw['차량사용여부'] == 1) & (cms_raw['DEV_EUI'] == 0) & ~(cms_raw['서비스유형'] == 8) ]
-      st.write('검증 대상 차량리스트 :', car_check_list['carId'].count())
-      st.dataframe(car_check_list)
-      st.write("---")
-      # carId 기준 정렬
-      car_available_sort = car_available.sort_values(by=['carId'])
-      # carId 중복검증하기
-      car_available_sort['carId중복'] = car_available_sort.duplicated(subset='carId', keep='last')
-      carId_dup_count = car_available_sort['carId중복'].sum()
-      carId_dup = car_available_sort.loc[car_available_sort['carId중복'] == True]
-      st.write('[검증필요]carId중복 차량대수:', carId_dup_count)
-      st.dataframe(carId_dup)
-      # carId 중복없는 차량리스트
-      carId_unique = car_available_sort.loc[car_available_sort['carId중복'] == False]
-      # 차량번호 중복데이터 찾기, 최근 carid값을 남김, 중복값은 true로 표기
-      carId_unique['차량번호중복'] = carId_unique.duplicated(subset='차량번호(clean)', keep=False)
-      # 원본 차량번호와 cleansing한 차량번호가 동일한 carId는 중복 아닌 것으로 변경
-      carId_unique.loc[(carId_unique['차량번호'] == carId_unique['차량번호(clean)']), '차량번호중복'] = False
-      st.write("---")
-      # 차량번호 중복 리스트 
-      car_duplicate = carId_unique.loc[carId_unique['차량번호중복'] == True]
-      st.write('[검증필요]차량번호 중복 리스트: ', car_duplicate['carId'].count())
-      st.dataframe(car_duplicate)
-      st.write("---")
-      # 차량번호 중복제거 리스트
-      car_unique = carId_unique.loc[carId_unique['차량번호중복'] == False]
-      st.write('유효한 차량리스트 :', car_unique['carId'].count())
-      st.dataframe(car_unique)
-      st.write("---")
-      #12월 장착차량
-      d = st.date_input('#### 장착시작일자 입력 ####', value=None)
-      st.write('장착시작일:', d)
+if st.button("CMS분석하기"):
+    #시간 속성 필드값 datetime으로 type 변경
+    cms_raw['차량생성일시'] = cms_raw['차량생성일시'].astype('datetime64[ns]')
+    cms_raw['현재장착일'] = cms_raw['현재장착일'].astype('datetime64[ns]')
+    cms_raw['마지막시동on'] = cms_raw['마지막시동on'].astype('datetime64[ns]')
+    cms_raw['DEV_EUI'].fillna(0,inplace=True)
+    cms_raw['차량번호(clean)'] = cms_raw['차량번호'].apply(carnoclean)
+
+    # 유효한 차량 - 차량사용여부 '1', 단말기 일련번호 있는 차량과 HMG API(서비스유형 '8') 조건
+    car_available = cms_raw.loc[((cms_raw['차량사용여부'] == 1) & (cms_raw['DEV_EUI'] != 0)) | (cms_raw['서비스유형'] == 8) ]
+    st.write('유효한 차량대수:' , car_available['carId'].count())
+    st.write('차량사용여부, 단말기 장착, HMG API 차량 포함')
+    st.write("---")
+    car_check_list = cms_raw.loc[(cms_raw['차량사용여부'] == 1) & (cms_raw['DEV_EUI'] == 0) & ~(cms_raw['서비스유형'] == 8) ]
+    st.write('검증 대상 차량리스트 :', car_check_list['carId'].count())
+    st.dataframe(car_check_list)
+    st.write("---")
+    # carId 기준 정렬
+    car_available_sort = car_available.sort_values(by=['carId'])
+    # carId 중복검증하기
+    car_available_sort['carId중복'] = car_available_sort.duplicated(subset='carId', keep='last')
+    carId_dup_count = car_available_sort['carId중복'].sum()
+    carId_dup = car_available_sort.loc[car_available_sort['carId중복'] == True]
+    st.write('[검증필요]carId중복 차량대수:', carId_dup_count)
+    st.dataframe(carId_dup)
+    # carId 중복없는 차량리스트
+    carId_unique = car_available_sort.loc[car_available_sort['carId중복'] == False]
+    # 차량번호 중복데이터 찾기, 최근 carid값을 남김, 중복값은 true로 표기
+    carId_unique['차량번호중복'] = carId_unique.duplicated(subset='차량번호(clean)', keep=False)
+    # 원본 차량번호와 cleansing한 차량번호가 동일한 carId는 중복 아닌 것으로 변경
+    carId_unique.loc[(carId_unique['차량번호'] == carId_unique['차량번호(clean)']), '차량번호중복'] = False
+    st.write("---")
+    # 차량번호 중복 리스트 
+    car_duplicate = carId_unique.loc[carId_unique['차량번호중복'] == True]
+    st.write('[검증필요]차량번호 중복 리스트: ', car_duplicate['carId'].count())
+    st.dataframe(car_duplicate)
+    st.write("---")
+    # 차량번호 중복제거 리스트
+    car_unique = carId_unique.loc[carId_unique['차량번호중복'] == False]
+    st.write('유효한 차량리스트 :', car_unique['carId'].count())
+    st.dataframe(car_unique)
+    st.write("---")
+    #12월 장착차량
+    d = st.date_input('#### 장착시작일자 입력 ####', value=None)
+    st.write('장착시작일:', d)
 
     if d is not None:
       date = np.datetime64(d) # 입력받은 날짜는 date type으로 datetime64 로 변환해야 날짜 비교 가능
@@ -203,9 +206,6 @@ if uploaded_file is not None:
       st.write("---")
     else:
       st.warning('기준일자를 입력 하세요')  
-
-else:
-    st.warning('엑셀파일을 업로드 하세요')
 
 
 uploaded_file2 = st.file_uploader('#### 2. 거래처별 영업채널 엑셀파일을 업로드하세요 ####')
